6ae00935cc70b79700de5033b409985b84e304be
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-07-04 23:42:16 +0200

    WIP: Kéchichian test case does not converge

--- test_edelbaum.py
+++ test_edelbaum.py
@@ -3,8 +3,11 @@ from astropy import units as u
 from numpy.testing import assert_almost_equal
 
 from poliastro.bodies import Earth
+from poliastro.twobody import State
 
-from edelbaum import extra_quantities
+from poliastro.twobody.propagation import cowell
+
+from edelbaum import guidance_law, extra_quantities
 
 
 def test_leo_geo_time_and_delta_v():
@@ -23,3 +26,42 @@ def test_leo_geo_time_and_delta_v():
 
     assert_almost_equal(t_f / 86400, expected_t_f, decimal=2)
     assert_almost_equal(delta_V, expected_delta_V, decimal=4)
+
+
+def test_leo_geo_time_history():
+    a_0 = 7000.0  # km
+    a_f = 42166.0  # km
+    i_f = 0.0  # deg
+    i_0 = (28.5 * u.deg).to(u.rad).value  # rad
+    f = 3.5e-7  # km / s2
+
+    k = Earth.k.decompose([u.km, u.s]).value
+
+    edelbaum_accel = guidance_law(k, a_0, a_f, i_f - i_0, f)
+
+    delta_V, t_f = extra_quantities(k, a_0, a_f, i_f - i_0, f)
+
+    # Retrieve r and v from initial orbit
+    s0 = State.circular(Earth, a_0 * u.km - Earth.R, i_0 * u.rad)
+    r0, v0 = s0.rv()
+
+    # Propagate orbit
+    # All the combinations of parameters fail when t = 7086 is reached
+    r, v = cowell(k,
+                  r0.to(u.km).value,
+                  v0.to(u.km / u.s).value,
+                  t_f,
+                  #dt=0.1,  # To integrate in a loop
+                  ad=edelbaum_accel,
+                  #nsteps=1000000)  # NEVER FINISHES
+                  nsteps=10000000,  # Does not converge
+                  rtol=1e-5)
+
+    sf = State.from_vectors(Earth,
+                            r * u.km,
+                            v * u.km / u.s,
+                            s0.epoch + t_f * u.s)
+
+    assert_almost_equal(sf.a.to(u.km).value, a_f)
+    assert_almost_equal(sf.inc.to(u.deg).value, i_f)
+    assert_almost_equal(sf.ecc.value, 0.0)
