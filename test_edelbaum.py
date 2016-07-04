370507fc48636417a10e4075917783169f3653c3
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-07-04 23:41:06 +0200

    Fix unit error, improve precision

--- test_edelbaum.py
+++ test_edelbaum.py
@@ -10,8 +10,8 @@ from edelbaum import extra_quantities
 def test_leo_geo_time_and_delta_v():
     a_0 = 7000.0  # km
     a_f = 42166.0  # km
-    i_f = 0.0  # deg
-    i_0 = 28.5  # deg
+    i_f = 0.0  # rad
+    i_0 = (28.5 * u.deg).to(u.rad).value  # rad
     f = 3.5e-7  # km / s2
 
     k = Earth.k.decompose([u.km, u.s]).value
@@ -21,5 +21,5 @@ def test_leo_geo_time_and_delta_v():
 
     delta_V, t_f = extra_quantities(k, a_0, a_f, i_f - i_0, f)
 
-    assert_almost_equal(t_f / 86400, expected_t_f, decimal=0)
-    assert_almost_equal(delta_V, expected_delta_V, decimal=1)
+    assert_almost_equal(t_f / 86400, expected_t_f, decimal=2)
+    assert_almost_equal(delta_V, expected_delta_V, decimal=4)
